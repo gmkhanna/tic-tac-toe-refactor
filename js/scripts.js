7b99dea52dfcb4748c27b4537f3bee2dafd311ce
grinil khanna and Shane Villars <grinil@>
2017-01-25 11:35:04 -0800

    Add functionality of animations for each square. All working correctly. Next Steps: add reset button, and begin backend logic.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@
 
 
 $(document).ready(function(){
+
     $("#x11").click(function(){
         $("#o11").hide();
         $("#x11").animate({
@@ -23,4 +24,137 @@ $(document).ready(function(){
             width: '100%'
         });
     });
+    $("#x12").click(function(){
+        $("#o12").hide();
+        $("#x12").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o12").click(function(){
+        $("#x12").hide();
+        $("#o12").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#x13").click(function(){
+        $("#o13").hide();
+        $("#x13").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o13").click(function(){
+        $("#x13").hide();
+        $("#o13").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+// END --- of the first row; START second Row
+
+    $("#x21").click(function(){
+        $("#o21").hide();
+        $("#x21").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o21").click(function(){
+        $("#x21").hide();
+        $("#o21").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#x22").click(function(){
+        $("#o22").hide();
+        $("#x22").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o22").click(function(){
+        $("#x22").hide();
+        $("#o22").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#x23").click(function(){
+        $("#o23").hide();
+        $("#x23").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o23").click(function(){
+        $("#x23").hide();
+        $("#o23").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+// End of the second row --- Begin third Row
+
+    $("#x31").click(function(){
+        $("#o31").hide();
+        $("#x31").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o31").click(function(){
+        $("#x31").hide();
+        $("#o31").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#x32").click(function(){
+        $("#o32").hide();
+        $("#x32").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o32").click(function(){
+        $("#x32").hide();
+        $("#o32").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#x33").click(function(){
+        $("#o33").hide();
+        $("#x33").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+    $("#o33").click(function(){
+        $("#x33").hide();
+        $("#o33").animate({
+
+            height: '100%',
+            width: '100%'
+        });
+    });
+
 });
