b0aecad4d99960b6693f755587f0b751cacc32db
grinil khanna and Shane Villars <grinil@>
2017-01-26 15:03:03 -0800

    add player 1 choice and current player attached

--- js/scripts.js
+++ js/scripts.js
@@ -23,8 +23,7 @@ Player.prototype.check = function() {
 }
 var playerX = new Player("x");
 var playerO = new Player("o");
-var currentPlayer = playerO;
-// var nextPlayer;
+var currentPlayer;
 
 var switchPlayer = function() {
   if (currentPlayer.symbol === "o") {
@@ -36,103 +35,130 @@ var switchPlayer = function() {
 
 $(document).ready(function(){
 
-
-    $("#sq11").click(function(){
-        currentPlayer.choices.push(11)
-        $("#" + currentPlayer.symbol + "11").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "11").hide();
+  $("#x1").click(function(){
+    currentPlayer = playerX;
+    $("#o1").hide();
+    $("#x1").animate({
+      height: '10%',
+      width: '40%',
     });
-
-    $("#sq12").click(function(){
-        currentPlayer.choices.push(12)
-        $("#" + currentPlayer.symbol + "12").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "12").hide();
+    $("#x2").hide();
+    $("#o2").animate({
+      height: '10%',
+      width: '40%',
     });
+  });
 
-    $("#sq13").click(function(){
-        currentPlayer.choices.push(13)
-        $("#" + currentPlayer.symbol + "13").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "13").hide();
+  $("#o1").click(function(){
+    currentPlayer = playerO;
+    $("#x1").hide();
+    $("#o1").animate({
+      height: '10%',
+      width: '40%',
     });
+    $("#o2").hide();
+    $("#x2").animate({
+      height: '10%',
+      width: '40%',
+    });
+  });
 
-    $("#sq21").click(function(){
-        currentPlayer.choices.push(21)
-        $("#" + currentPlayer.symbol + "21").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "21").hide();
+  $("#sq11").click(function(){
+    currentPlayer.choices.push(11)
+    $("#" + currentPlayer.symbol + "11").animate({
+      height: '100%',
+      width: '100%',
     });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "11").hide();
+  });
 
-    $("#sq22").click(function(){
-        currentPlayer.choices.push(22)
-        $("#" + currentPlayer.symbol + "22").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "22").hide();
+  $("#sq12").click(function(){
+    currentPlayer.choices.push(12)
+    $("#" + currentPlayer.symbol + "12").animate({
+      height: '100%',
+      width: '100%',
     });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "12").hide();
+  });
 
-    $("#sq23").click(function(){
-        currentPlayer.choices.push(23)
-        $("#" + currentPlayer.symbol + "23").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "23").hide();
+  $("#sq13").click(function(){
+    currentPlayer.choices.push(13)
+    $("#" + currentPlayer.symbol + "13").animate({
+      height: '100%',
+      width: '100%',
     });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "13").hide();
+  });
 
-    $("#sq31").click(function(){
-        currentPlayer.choices.push(31)
-        $("#" + currentPlayer.symbol + "31").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "31").hide();
+  $("#sq21").click(function(){
+    currentPlayer.choices.push(21)
+    $("#" + currentPlayer.symbol + "21").animate({
+      height: '100%',
+      width: '100%',
     });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "21").hide();
+  });
 
-    $("#sq32").click(function(){
-        currentPlayer.choices.push(32)
-        $("#" + currentPlayer.symbol + "32").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "32").hide();
+  $("#sq22").click(function(){
+    currentPlayer.choices.push(22)
+    $("#" + currentPlayer.symbol + "22").animate({
+      height: '100%',
+      width: '100%',
     });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "22").hide();
+  });
 
-    $("#sq33").click(function(){
-        currentPlayer.choices.push(33)
-        $("#" + currentPlayer.symbol + "33").animate({
-            height: '100%',
-            width: '100%',
-        });
-        currentPlayer.check();
-        switchPlayer();
-        $("#" + currentPlayer.symbol + "33").hide();
+  $("#sq23").click(function(){
+    currentPlayer.choices.push(23)
+    $("#" + currentPlayer.symbol + "23").animate({
+      height: '100%',
+      width: '100%',
     });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "23").hide();
+  });
+
+  $("#sq31").click(function(){
+    currentPlayer.choices.push(31)
+    $("#" + currentPlayer.symbol + "31").animate({
+      height: '100%',
+      width: '100%',
+    });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "31").hide();
+  });
+
+  $("#sq32").click(function(){
+    currentPlayer.choices.push(32)
+    $("#" + currentPlayer.symbol + "32").animate({
+      height: '100%',
+      width: '100%',
+    });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "32").hide();
+  });
+
+  $("#sq33").click(function(){
+    currentPlayer.choices.push(33)
+    $("#" + currentPlayer.symbol + "33").animate({
+      height: '100%',
+      width: '100%',
+    });
+    currentPlayer.check();
+    switchPlayer();
+    $("#" + currentPlayer.symbol + "33").hide();
+  });
 });
